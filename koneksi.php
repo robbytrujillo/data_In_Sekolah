01b4afd54b4c9c258f530e57e2711d73818f3822
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-11 23:03:33 +0700

    Web Data internal Sekolah

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
     $koneksi = mysql_connect("localhost","root","");
     if($koneksi){
-        mysql_select_db("dbinsekolah");
+        mysql_select_db("dbInSekolah");
     } else {
         echo "Koneksi Gagal!";
     }
